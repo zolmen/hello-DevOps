af167b29832b49c726c6ace4304fddd0f3ca96b9
Kárpáti Zoltán <zolmen@ax.karpatilabor.com>
2025-12-04 15:30:55 +0100

    Add /info endpoint

--- src/HelloDevOps/Program.cs
+++ src/HelloDevOps/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello DevOps from feature branch");
+app.MapGet("/", () => "Hello DevOps");
+app.MapGet("/info", () => "Hello DevOps info endpoint");
 
 app.Run("http://0.0.0.0:8080");
